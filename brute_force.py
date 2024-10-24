6dbeb3a6fa9da2fab1079621140f59bd4cfa8ee0
TumeloMkwambe <2446873@students.wits.ac.za>
2024-10-24 17:27:26 +0200

    Closest Pair Complete

--- brute_force.py
+++ brute_force.py
@@ -34,37 +34,37 @@ def findClosestPair(points):
     return closestPair(points)
 
 def closestPair(points):
-    if(len(points) == 2):
-        distance = np.linalg.norm(points[1] - points[0])
-        if(points[0][1] < points[1][1]):
+    if(len(points) == 2): # If there are 2 points in the set of points
+        distance = np.linalg.norm(points[1] - points[0]) # Calculate d directly
+
+        if(points[0][1] < points[1][1]): # Arrange the two points into a list Y sorted on y coordinate
             return distance, points
         else:
             Y = points
             return distance, Y[::-1]
     else:
-        P_L = points[0 : len(points) // 2]
+        P_L = points[0 : len(points) // 2] # Divide the set into two equal-sized parts PL and PR
         P_R = points[len(points) // 2 : ]
         d1, YL = closestPair(P_L)
         d2, YR = closestPair(P_R)
-        Y = np.concatenate((YL, YR), axis=0)
+        Y = np.concatenate((YL, YR), axis=0) # Merge the two sorted lists YL and YR into one sorted list Y
         Y = Y[Y[:, 1].argsort()]
         distance = np.min(np.array([d1, d2]))
         vert_line = (P_L[len(P_L) - 1][0] + P_R[0][0]) / 2
         S = list()
         for i in range(len(Y)):
-            if(abs(Y[i][0] - vert_line) < distance):
-                S.append(Y[i])
+            if(abs(Y[i][0] - vert_line) < distance): # Let S be the points in Y which are in the strip
+                S.append(Y[i]) 
         S = np.array(S)
         for i in range(len(S) - 1):
             j = 1
             while(i+j < len(S) and j <= 7):
-                print(f"i = {i} and j = {j}")
-                print(f"S = {np.linalg.norm(S[i] - S[i + j])}")
+                if(np.linalg.norm(S[i] - S[i + j]) < distance):
+                    distance = np.linalg.norm(S[i] - S[i + j])
                 j += 1
         print(f"S: {S}")
 
         return distance, Y
-
 
 def timer(function, points):
     start = time.time()
